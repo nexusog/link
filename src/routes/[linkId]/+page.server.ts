1582e8ba337fce3daa3b2f351402ce3170b1de22
Henil Malaviya <henilm.dev@gmail.com>
2025-02-25 19:08:30 +0530

    feat: functioning redirect behaviour

---
+++ src/routes/[linkId]/+page.server.ts
@@ -0,0 +1,16 @@
+import { redirect } from '@sveltejs/kit'
+import type { PageServerLoad } from './$types'
+import { PUBLIC_API_BASE_URL } from '$env/static/public'
+
+export const load: PageServerLoad = (event) => {
+	const { params, url } = event
+
+	const { linkId } = params
+
+	const type = url.searchParams.has('qr') ? 'QR' : 'CLICK'
+
+	return redirect(
+		307,
+		`${PUBLIC_API_BASE_URL}/links/${linkId}/redirect?type=${type}`,
+	)
+}
